29608d00daec469034c77148273c9ee248f7f1d3
nanoky <konancharlesyao@gmail.com>
2024-02-07 07:14:25 +0000

    product page card position issue fixed

--- app/produit/[slug]/page.tsx
+++ app/produit/[slug]/page.tsx
@@ -67,7 +67,7 @@ export default async function ProductPage({
     }
 
     return (
-        <Layout store={store}>
+        <Layout store={store} hasFooter={false}>
             <div className="d-flex flex-column gap-3 px-product w-100 py-4">
                 <div className="d-flex justify-content-center flex-row align-items-center gap-2">
                     <Breadcrumbs
