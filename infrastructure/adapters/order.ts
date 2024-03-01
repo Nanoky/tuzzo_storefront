ff443d8f837a0f4dbe123d1cf4f21f4fc32a0f44
nanoky <konancharlesyao@gmail.com>
2024-03-01 10:38:44 +0000

    order customer phone number save undefined issue fixed

--- infrastructure/adapters/order.ts
+++ infrastructure/adapters/order.ts
@@ -18,7 +18,7 @@ export class OrderCustomerConverter
     toFirestore(modelObject: OrderCustomer): OrderCustomerDTO {
         return {
             name: modelObject.fullname,
-            phone_number: `${modelObject.phoneIndicator}${modelObject.phone}`,
+            phone_number: `${modelObject.phone}`,
             delivery_address: modelObject.address,
             notes: "Client enregistré depuis le web",
             created_at: new Date(),
